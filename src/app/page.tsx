f9e8a9429472cd05de6a6fcbb124dd834daef6dc
Jon Pritchard <jon@node28.com>
2023-11-26 13:05:30 +0000

    Adding auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,8 +18,8 @@ const authOptions = {
 }
 
 export default async function Home() {
-  // const { auth } = NextAuth(authOptions);
-  // const session = await auth()
+  const { auth } = NextAuth(authOptions);
+  const session = await auth()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
